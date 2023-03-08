b8cb4e2f452ab2f6b196032b079b81b5f807d03a
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-09 03:53:07 +0700

    add participants info get competition api

--- src/BirdTournaments.Web/Endpoints/CompetitionEndpoints/CompetitionRecord.cs
+++ src/BirdTournaments.Web/Endpoints/CompetitionEndpoints/CompetitionRecord.cs
@@ -2,12 +2,27 @@
 
 namespace BirdTournaments.Web.Endpoints.CompetitionEndpoints;
 
-public record CompetitionRecord(int Id, DateTime date, string place, string birdType, int elo, string status, string challengerName, string image)
+public record CompetitionRecord(int Id, DateTime date, string place, string birdType, int elo, string status, string challengerName, string image, IEnumerable<ParticipantRecord> participantRecords)
 {
   public static CompetitionRecord FromEntity(Competition competition)
   {
     var participant = competition.Participants.First()!;
-    var competitionRecord = new CompetitionRecord(competition.Id, competition.Date, competition.Place.Address, competition.BirdType.Name, participant.Bird.Elo, competition.Status.Name, participant.BirdOwner.Name, participant.Bird.Image);
+
+    var participantRecords = competition.Participants.Select(ParticipantRecord.FromEntity);
+
+    var competitionRecord = new CompetitionRecord(
+      competition.Id, 
+      competition.Date, 
+      competition.Place.Address, 
+      competition.BirdType.Name, 
+      participant.Bird.Elo, 
+      competition.Status.Name, 
+      participant.BirdOwner.Name,
+      participant.Bird.Image,
+      participantRecords);
+
+
+
 
     return competitionRecord;
   }
---
+++ src/BirdTournaments.Web/Endpoints/CompetitionEndpoints/ParticipantRecord.cs
@@ -0,0 +1,11 @@
+﻿using BirdTournaments.Core.ParticipantAggregate;
+
+namespace BirdTournaments.Web.Endpoints.CompetitionEndpoints;
+
+public record ParticipantRecord(int ownerId, string ownerName, bool isWin)
+{
+  public static ParticipantRecord FromEntity(Participant participant)
+  {
+    return new ParticipantRecord(participant.BirdOwner.Id, participant.BirdOwner.Name, participant.Status.Name == ParticipantStatus.Win.Name);
+  }
+}
