c1133e435f0364240bcdaa14a8ccaabda45c6750
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-13 21:47:44 +0700

    add image of bird record

--- src/BirdTournaments.Web/Endpoints/MeEndPoints/BirdRecord.cs
+++ src/BirdTournaments.Web/Endpoints/MeEndPoints/BirdRecord.cs
@@ -2,10 +2,10 @@
 
 namespace BirdTournaments.Web.Endpoints.MeEndPoints;
 
-public record BirdRecord(int id, string name, int elo, string rankName)
+public record BirdRecord(int id, string name, int elo, string rankName, string image)
 {
   public static BirdRecord FromEntity(Bird bird)
   {
-    return new BirdRecord(bird.Id, bird.Name, bird.Elo, bird.Rank.Name);
+    return new BirdRecord(bird.Id, bird.Name, bird.Elo, bird.Rank.Name, bird.Image);
   }
 }
